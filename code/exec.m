4a2a73777623b0facbc9d1307a4170381bc15ca6
Domenico <55285326+mimmo96@users.noreply.github.com>
2023-05-21 17:47:22 +0200

    Update exec.m

--- code/exec.m
+++ code/exec.m
@@ -1,23 +1,24 @@
 m = 10;
-n = 8;
-rank_k = 7;
+n = 7;
+rank_k = 3;
 
-A = rand(m,n);
-num_it = 10;
+A = rand(m,n)*1e-12;
+tol = 0.001;
+num_it = 100;
 [u,s,v] = svd(A);
 
-fprintf('rank \t\t\t altern_err \t\t svd_err \n')
+fprintf('rank \t\t altern_err \t\t svd_err \n')
 
 for k=1:rank_k
     
-    U = rand(m,k);
-    V = rand(n,k);
+    U = rand(m,k)*1e-04;
+    V = rand(n,k)*1e-04;
 
-    altern = alternating_optimization(A,U,V,num_it);
+    altern = alternating_optimization(A,U,V,tol,num_it);
     Ak = u(:,1:k)*s(1:k,1:k)*v(:,1:k)'; 
    
     svd_err= norm(A-Ak, 'fro');
 
-    fprintf('%f \t\t %f \t\t %f \n', k,  altern, svd_err)
+    fprintf('%d \t\t %.5e \t\t %.5e \n', k,  altern, svd_err)
 
-end
+end 
