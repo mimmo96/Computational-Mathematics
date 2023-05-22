9a3fb584b71199a5b41d8ade2e2f9bbab8448f9c
Domenico <55285326+mimmo96@users.noreply.github.com>
2023-05-22 17:37:16 +0200

    Create make_graph.m

---
+++ code/make_graph.m
@@ -0,0 +1,9 @@
+function make_graph(x, y)
+    % Traccia il grafico dei valori
+    plot(x, y, 'o-');
+
+    % Aggiungi etichette agli assi e un titolo al grafico
+    xlabel('X');
+    ylabel('Y');
+    title('Graph');
+end
