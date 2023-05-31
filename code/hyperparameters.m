97add01eb4e21ff4242e5fece50762f436ce6c12
domenico profumo <domenico.profumo@gmail.com>
2023-05-31 10:26:50 +0200

    updated hyperparameters

--- code/hyperparameters.m
+++ code/hyperparameters.m
@@ -9,14 +9,14 @@ m = 100;
 n = 10; 
 
 %
-step_col = 10;
+step_col = 100;
 step_rows = 1000;
 
-max_col = 50;
+max_col = 1000;
 max_rows = 10000;
 
 %magnitude of values
-magnitude = [1, 1e-10, 1e10];
+magnitude = [1, 1e-14, 1e14];
 
 %matrici sparse
 %density = [0.25, 0.5, 0.75];
@@ -26,7 +26,7 @@ num_iterations = 1000;
 error = 1e-16;
 
 %gap tollerance
-tol = 1e-15;
+tol = 1e-04;
 
 %rank 
 rank = [0.25, 0.5, 0.75, 1];
