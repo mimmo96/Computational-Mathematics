505fff4de28d45e57364d76818fc36beba034fc5
Domenico Profumo <domenico.profumo@gmail.com>
2023-07-10 11:01:45 +0200

    add other comments

--- code/utils/graph/interpolation.m
+++ code/utils/graph/interpolation.m
@@ -1,24 +1,21 @@
-%rows
+% script for interpolation function
+%
+% Author:   Domenico Profumo 
+%           Gerlando Gramaglia
+%
+
+
+%values of x axis
 x = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
 
-rank_25 = [1.111640e-01, 4.586030e-01, 1.269365e+00, 2.552912e+00, 5.032398e+00, 8.134306e+00, 1.224648e+01, 1.651475e+01, 2.370004e+01, 3.364446e+01 ];
-rank_50 = [2.615410e-01, 1.194009e+00, 3.834570e+00, 8.653561e+00, 1.564082e+01, 2.544865e+01, 4.500244e+01, 6.157173e+01, 8.838664e+01, 1.163819e+02 ];
-rank_75 = [4.652930e-01, 2.852418e+00, 8.422085e+00, 1.934301e+01, 3.776819e+01, 6.556215e+01, 8.864838e+01, 1.307613e+02, 1.910159e+02, 2.663084e+02 ];
+%values of y axis
+y = [3.900441e-01, 4.278068e-01,5.402864e-01,6.936480e-01,8.396877e-01,8.901296e-01,1.037936e+00,1.196557e+00,1.320439e+00,1.452170e+00];
 
-rank_50_50col = [3.900441e-01, 4.278068e-01,5.402864e-01,6.936480e-01,8.396877e-01,8.901296e-01,1.037936e+00,1.196557e+00,1.320439e+00,1.452170e+00];
+% Found and print polynomial regression coefficients
+coeff = polyfit(x, y, 1)
 
-%plot(x, rank_25);
-%hold on;
-%plot(x, rank_50);
-%hold on;
-%plot(x, rank_75);
-%hold off;
-
-y = rank_50_50col;
-coeff = polyfit(x, y, 1); % Polynomial regression coefficients
-y_fit = polyval(coeff, x); % Estimated values of polynomial regression
-
-coeff
+% Estimated values of polynomial regression
+y_fit = polyval(coeff, x);
 
 %plot real values
 plot(x, y);
@@ -28,9 +25,9 @@ hold on;
 plot(x, y_fit);
 xlabel('rows/columns');
 ylabel('time(s)');
-title('Comparison between real fun and predicted polynomial with 50 columns and rank=25');
+title('Comparison between real fun and predicted polynomial');
 
-legend('real fun','1° degree polynomial');
+legend('real fun','polynomial interpolation');
 grid on;
 
 hold off;
--- code/utils/graph/read_file_for_graph.m
+++ code/utils/graph/read_file_for_graph.m
@@ -1,41 +1,72 @@
+% script for make graph from log_stats files
+%
+% Author:   Domenico Profumo 
+%           Gerlando Gramaglia
+%
+
 format long e;
 
-path = '../results/log_stats.txt';
+% NOTE: change it manualy!
+% ----- OPTIONS -----
 
-M = dlmread(path);
+% path of the folder that contain log_stats.txt results in this case is considered in
+% the current path
+path = 'log_stats.txt';
 
-matrix = M(1:150,1:end);
+%a 150-line file is used
+num_rows = 150;
+
+% -------------------
+
+%read the contents of the file by deleting the first line
+fileContent = fileread(path);
+newlineIdx = strfind(fileContent, newline);
+fileContent = fileContent(newlineIdx(1)+1:end);
+tempFile = tempname;
+fid = fopen(tempFile, 'w');
+fwrite(fid, fileContent, 'char');
+fclose(fid);
+data = dlmread(tempFile);
+delete(tempFile);
+
+%create a matrix with file content
+M = data;
+matrix = M(1:num_rows,1:end);
 
 %set rank for graph:
 % 0 = 75%
 % 1 = 50%
 % 2 = 25%
 
+dim_rank = num_rows/3;
 rank = 0;
 
 % grafico del tempo rispetto al numero delle righe (ogni linea rappresenta
 % la colonna)
-x_val = Inf(1, 50);
-y_val = Inf(1, 50);
+x_val = Inf(1, dim_rank);
+y_val = Inf(1, dim_rank);
 
-for i=1:50
+% store the values for different rank
+for i=1:dim_rank
     x_val(i) = M((i*3)-rank,2);
     y_val(i) = M((i*3)-rank,11);
 end
 
+% x_column, one for each size
 xcolumn_1 = x_val(1:10);
 xcolumn_2 = x_val(11:20);
 xcolumn_3 = x_val(21:30);
 xcolumn_4 = x_val(31:40);
 xcolumn_5 = x_val(41:50);
 
+% y_column, one for each size
 ycolumn_1 = y_val(1:10);
 ycolumn_2 = y_val(11:20);
 ycolumn_3 = y_val(21:30);
 ycolumn_4 = y_val(31:40);
 ycolumn_5 = y_val(41:50);
 
-% Plot 
+% Plot graph
 plot(xcolumn_1, ycolumn_1,'m--')
 hold on;
 plot(xcolumn_2, ycolumn_2,'r--')
@@ -49,4 +80,3 @@ ylabel('time(s)');
 grid on;
 
 hold off;
-
